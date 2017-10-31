65edc00abee9826575fb107790452ef54624fd79
Alex Gillis <ajg468@jazz.cs.mun.ca>
2017-10-31 11:12:59 -0230

    Finished student.html display

--- UniversitySite_A1/WebContent/JS/registration.js
+++ UniversitySite_A1/WebContent/JS/registration.js
@@ -1,10 +1,54 @@
 /**
  * Enrolls a student in the selected course.
  * Currently assumes there is only one student in the school, as per assignment instructions.
+ * A Student currently cannot unregister from a course as it was not in the assignment requirements.
  * 
  * courseArray stores the keys for the actual course registration data
  * @author Alex Gillis 
  */
+
+//Called on student.html onload
+function displayRegisteredCourses(){
+	
+	var courseArray = getCourseArray();
+	var courseTbl = document.getElementById("studentCourseList");
+
+	//Destory Previous Table rows
+	while(courseTbl.hasChildNodes()){
+		courseTbl.removeChild(courseTbl.lastChild);
+	}
+
+	//Create Table Headers
+	var tblHeaderTxt = ["Course Number", "Time Slot"];
+	var headerRow = document.createElement("tr");
+	for(var x = 0; x < tblHeaderTxt.length; x++){
+		
+		var headerth = document.createElement('th');
+		var textNode = document.createTextNode(tblHeaderTxt[x]);
+		headerth.appendChild(textNode);
+		headerRow.appendChild(headerth);
+	}
+	courseTbl.appendChild(headerRow);
+
+	//Add Course Entries to the table
+	for(var i = 0; i < courseArray.length; i++){
+		var courseRow = document.createElement("tr");
+		var key = courseArray[i];
+		var courseInfo = JSON.parse(localStorage[key]);
+		
+
+		var course_td1 = document.createElement('td');
+		var txtNode1 = document.createTextNode(courseInfo.courseNum);
+		course_td1.appendChild(txtNode1);
+		courseRow.appendChild(course_td1);
+		var course_td2 = document.createElement('td');
+		var txtNode2 = document.createTextNode(courseInfo.sectionNum);
+		course_td2.appendChild(txtNode2);
+		courseRow.appendChild(course_td2);
+
+		courseTbl.appendChild(courseRow);
+	}	
+}
 
 function registerBtnClicked(){
 	
@@ -21,6 +65,17 @@ function registerBtnClicked(){
 	alert("You have registered for " + courseNum);
 }
 
+function stuPageRegisterBtn(){
+	
+	var courseTxt = document.getElementById("courseNum").value;
+	var sectionTxt = document.getElementById("timeSlot").value;
+
+	var courseInfo = new course(courseTxt,sectionTxt);
+	
+	saveCourseInfo(courseInfo);
+	displayRegisteredCourses();
+}
+
 function saveCourseInfo(course){
 	
 	var key = course.courseNum + course.sectionNum;
@@ -28,6 +83,7 @@ function saveCourseInfo(course){
 	
 	if( addCourseArray(key) ){ //Returns true if successfully set
 		localStorage.setItem(key, value);
+		alert("You have registered for " + course.courseNum);
 		
 		//TESTING
 //		var courseArray = getCourseArray();
